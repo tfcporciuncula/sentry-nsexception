ef5a9338ccc58549c04b82fbb04b22054a29a797
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-06-06 13:41:22 +0200

    Add blank cocoapods KMM project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sentry_NSExceptionKt"
+include(":androidApp")
+include(":shared")
